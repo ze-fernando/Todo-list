af958c65fb922ae4056d318d8566c639a9b97594
ze-fernando <jfenginner935@gmail.com>
2024-03-28 19:18:15 -0300

    feat: implements CRUD for tasks

--- task/views.py
+++ task/views.py
@@ -1,25 +1,57 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import authentication, permissions
+from rest_framework.parsers import JSONParser
+from django.http import JsonResponse, HttpResponse
+
+from task.models import Task
+from .serializers import TaskSerializer
 
 
 class TaskList(APIView):
-    def get(self):
-        ...
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return JsonResponse(serializer.data, safe=False)
 
+    def post(self, req):
+        data = JSONParser().parse(req)
+        serializer = TaskSerializer(data=data)
 
-    def post(self):
-        ...
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, safe=False)
 
+        return JsonResponse(serializer.errors, status=400)
 
 
 class TaskDetail(APIView):
-    
-    def get(self, id):
-        ...
 
-    def put(self, id):
-        ...
+    def get(self, req, id):
+        try:
+            task = Task.objects.get(id=id)
+            serializer = TaskSerializer(task)
+            return JsonResponse(serializer.data)
+        except Task.DoesNotExist:
+            return HttpResponse({'message': 'user id not found'}, status=404)
 
-    def delete(self, id):
-        ...
+    def put(self, req, id):
+        try:
+            task = Task.objects.get(id=id)
+            data = JSONParser().parse(req)
+            serializer = TaskSerializer(task, data=data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data, safe=False)
+
+            return JsonResponse(serializer.errors, status=400)
+
+        except Task.DoesNotExist:
+            return HttpResponse({'message': 'user id not found'}, status=404)
+
+    def delete(self, req, id):
+        try:
+            task = Task.objects.get(id=id)
+            task.delete()
+            return JsonResponse({'message': 'task deleted'}, status=204)
+        except Task.DoesNotExist:
+            return HttpResponse({'message': 'user id not found'}, status=404)
