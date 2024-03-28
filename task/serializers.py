40d795774e5410a1c1ed87ccdad737d70e110ab6
ze-fernando <jfenginner935@gmail.com>
2024-03-28 19:17:27 -0300

    feat: create taskserializer

---
+++ task/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework.schemas.coreapi import serializers
+
+from task.models import Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = [
+            'name',
+            'created_at',
+            'user'
+        ]
