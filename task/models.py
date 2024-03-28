7059486f506bba96231293b6691c4824937b9376
ze-fernando <jfenginner935@gmail.com>
2024-03-28 00:14:44 -0300

    feat: create models user and task

--- task/models.py
+++ task/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
+from user.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    __tablename__ = 'tasks'
+
+
+    name=models.CharField(max_length=25)
+    done=models.BooleanField()
+    created_at=models.DateField(default=datetime.now())
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_name=models.CharField(max_lenght=25)
+    password=models.CharField()
+    age=models.IntegerField()
+    
