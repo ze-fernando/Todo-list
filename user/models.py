a473caefc1e9206bfd10ad66530db8c343f5d08f
ze-fernando <jfenginner935@gmail.com>
2024-03-28 19:17:10 -0300

    feat: insert objects in user

--- user/models.py
+++ user/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, UserManager
 
 class User(AbstractBaseUser):
 
     user_name = models.TextField()
     password = models.TextField()
     age = models.IntegerField()
+
+    objects = UserManager()
 
     last_login = None
 
