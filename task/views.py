6c1e3ae1efe5877b70d3c3a6c1ad272e196cf30b
ze-fernando <jfenginner935@gmail.com>
2024-03-29 21:18:39 -0300

    feat: set permissions in class

--- task/views.py
+++ task/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.parsers import JSONParser
+from rest_framework.permissions import IsAuthenticated
 from django.http import JsonResponse, HttpResponse
 
 from task.models import Task
@@ -7,6 +8,8 @@ from .serializers import TaskSerializer
 
 
 class TaskList(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         tasks = Task.objects.all()
         serializer = TaskSerializer(tasks, many=True)
@@ -22,8 +25,11 @@ class TaskList(APIView):
 
         return JsonResponse(serializer.errors, status=400)
 
+#    return JsonResponse({'message': 'Unauthorized'}, status=401)
+
 
 class TaskDetail(APIView):
+    permission_classes = [IsAuthenticated]
 
     def get(self, req, id):
         try:
