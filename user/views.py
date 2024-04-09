f36f2cd405bd02d99a3e91d17d31085255363621
ze-fernando <jfenginner935@gmail.com>
2024-04-08 22:05:49 -0300

    bug: passworwd nao haseada

--- user/views.py
+++ user/views.py
@@ -2,13 +2,14 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User
 
 
 class UserLogin(APIView):
 
     def post(self, req):
-        username = req.data.get('user')
-        password = req.data.get('pass')
+        username = req.data.get('username')
+        password = req.data.get('password')
 
         user = authenticate(username=username, password=password)
 
@@ -26,4 +27,16 @@ class UserLogin(APIView):
 class UserCreate(APIView):
 
     def post(self, req):
-        ...
+        data = req.data
+        user = User.objects.filter(username=data['username']).first()
+
+        if user is not None:
+            return JsonResponse({
+                'error': 'username already exists'
+            }, status=400)
+
+        new_user = User.objects.create(**data)  # nao salva senha haseada
+        new_user.save()
+        return JsonResponse({
+            'message': 'user created successfully'  # temporario
+        })
