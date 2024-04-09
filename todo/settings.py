f5ea88e8250e8fe5887ac1f8cda159462c7395fe
ze-fernando <jfenginner935@gmail.com>
2024-04-08 22:04:06 -0300

    feat: inser auth_user_model

--- todo/settings.py
+++ todo/settings.py
@@ -93,11 +93,13 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = "user.User"
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-            'django.contrib.auth.password_validation\
-                .UserAttributeSimilarityValidator',
+            'django.contrib.auth.password_validation'
+        + '.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
