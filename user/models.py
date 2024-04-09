d47c623832ba9e9546aa3c2ccaf91dc36adaaea0
ze-fernando <jfenginner935@gmail.com>
2024-04-08 22:04:24 -0300

    feat: customize model

--- user/models.py
+++ user/models.py
@@ -1,15 +1,23 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, UserManager
+from django.contrib.auth.models import AbstractBaseUser
+from .manager import CustomUserManager
 
 
 class User(AbstractBaseUser):
 
-    user_name = models.TextField()
+    username = models.TextField(unique=True)
     password = models.TextField()
     age = models.IntegerField()
+    tel = models.IntegerField(unique=True, default=0)
 
-    objects = UserManager()
+    objects = CustomUserManager()
 
     last_login = None
 
-    USERNAME_FIELD = 'id'
+    USERNAME_FIELD = 'username'
+
+    def __str__(self):
+        return self.username
+
+    def __repr__(self):
+        return f"User:{self.username=} {self.age=} {self.tel=}"
