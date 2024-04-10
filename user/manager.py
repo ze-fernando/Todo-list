46c36f95c56b921f7a853e505a7ddb51a113e260
ze-fernando <jfenginner935@gmail.com>
2024-04-09 22:08:01 -0300

    feat: config user manager

--- user/manager.py
+++ user/manager.py
@@ -3,13 +3,13 @@ from django.utils.translation import gettext_lazy as gl
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, username, password, **kwargs):
+    def create_user(self, username, age, tel, password=None):
 
         if not username:
             raise ValueError(gl("The username must be set"))
 
         username = self.normalize_username(username)
-        user = self.model(username=username, **kwargs)
+        user = self.model(username=username, age=age, tel=tel)
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
         return user
