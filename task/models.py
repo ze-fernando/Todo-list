f51ae5cb8f25a6c8eb98c93c49eac1080c4a4fc3
ze-fernando <jfenginner935@gmail.com>
2024-04-20 13:00:13 -0300

    feat: finish

--- task/models.py
+++ task/models.py
@@ -7,3 +7,9 @@ class Task(models.Model):
     done = models.BooleanField(default=False)
     created_at = models.DateField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __repr__(self):
+        return f'Task {self.name=} {self.done=} {self.user=}'
+
+    def __str__(self):
+        return self.name
